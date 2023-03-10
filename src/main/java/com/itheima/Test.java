c36364e00f141d81a4381525d8591a8ea446ca63
Zorioon <127411763+Zorioon@users.noreply.github.com>
2023-03-10 09:51:12 +0800

    pull commit

--- src/main/java/com/itheima/Test.java
+++ src/main/java/com/itheima/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("hot-fix2");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
 }
