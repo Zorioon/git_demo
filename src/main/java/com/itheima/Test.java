903fa5b5fe84b605a780d1285ba12ff3b3bdd1bf
Zorion <acc123@rm.com>
2023-03-10 09:47:17 +0800

    fifth commit

--- src/main/java/com/itheima/Test.java
+++ src/main/java/com/itheima/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hot-fix2");
         System.out.println("push test");
+        System.out.println("push test2");
     }
 }
